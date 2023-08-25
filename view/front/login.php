94d623393e63a6a665c6f63a11971a10339a742e
jerrywu <s736828@gmail.com>
2023-08-25 16:35:53 +0800

    0825

---
+++ view/front/login.php
@@ -0,0 +1,44 @@
+<fieldset>
+    <legend>會員登入</legend>
+    <table>
+        <tr>
+            <td>帳號</td>
+            <td><input type="text" name="acc" id="acc"></td>
+        </tr>
+        <tr>
+            <td>密碼</td>
+            <td><input type="password" name="pw" id="pw"></td>
+        </tr>
+        <tr>
+            <td>
+                <input type="button" value="登入" onclick="login()">
+                <input type="button" value="清除" onclick="clean()">
+            </td>
+            <a href="?do=forgot">忘記密碼</a><a href="?do=reg">尚未註冊</a>
+        </tr>
+    </table>
+</fieldset>
+
+<script>
+    function login(){
+        let user={acc:$("#acc").val(),
+                  pw:$("#pw").val()}
+        $.post("./api/chk_acc.php",user,(res)=>{
+            switch(parseInt(res)){
+                case 0:
+                    alert("查無帳號")
+                break;
+                case 1:
+                    if(user.acc=='admin'){
+                        location.href='backend.php';
+                    }else{
+                        location.href='index.php';
+                    }
+                break;
+                case 2:
+                    alert("密碼錯誤")
+                break;
+            }
+        })
+    }
+</script>
---
+++ view/front/news.php
@@ -0,0 +1,14 @@
+<fieldset>
+    <legend>目前位置:首頁>最新文章區</legend>
+    <table>
+        <tr>
+            <td width="30%">標題</td>
+            <td width="40%">內容</td>
+            <td></td>
+        </tr>
+    <?php
+
+    ?>
+    </table>
+
+</fieldset>
